ed35c971d7cdad9980fbc5ad6a643a88e4226aa5
btinfo <38733928+btinfo@users.noreply.github.com>
2022-11-22 01:17:48 +0800

    u

--- asn/asn.sh
+++ asn/asn.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #
-sed -i 's/ $//' test
+
 sed -i 's/\t/\/\/ /' test
 sed -i 's/^AS/IP-ASN,/' test
+sed -i 's/ $//' test
 echo "# updated at $(date --rfc-3339=seconds)" > $1
 sort -V test >> $1
 rm -f test
