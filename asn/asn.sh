aaaec81a607d8c4ddcc431a92a477d1dddb72755
btinfo <38733928+btinfo@users.noreply.github.com>
2023-03-18 17:50:29 +0800

    Update asn.sh

--- asn/asn.sh
+++ asn/asn.sh
@@ -6,7 +6,7 @@ sed -i 's/^AS/IP-ASN,/' test
 sed -i 's/ $//' test
 echo "# $(date --rfc-3339=seconds)" > $1
 sort -V test >> $1
-if [[ "$1" = "china.list" ]]; then
+if [[ "$1" = "china" ]]; then
   echo "host-suffix,cn" >> $1
 else
   echo "host-suffix,kr" >> $1
