6d97c2e2e38e1d0f71b856a202caa03052532b92
btinfo <38733928+btinfo@users.noreply.github.com>
2025-09-14 01:34:21 +0800

    u

--- icon/icons.sh
+++ icon/icons.sh
@@ -1,5 +1,5 @@
 echo "{\"name\":\"$1\", \"description\":\"@btinfo\", \"icons\":[" > $1.json
 for i in `ls $1`
-  do echo "{\"name\":\"$i\",\"url\":\"https://raw.githubusercontent.com/btinfo/qx/main/icon/$1/$i\"}," >> $1.json
+  do echo "{\"name\":\"${i%.*}\",\"url\":\"https://raw.githubusercontent.com/btinfo/qx/main/icon/$1/$i\"}," >> $1.json
 done
 sed -i '$ s/.$/]}/' $1.json
